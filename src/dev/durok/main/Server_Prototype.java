9904e4471f4a33d4c709001c89b5c4ac4cc3f5c2
jihunahn <angh5080@gmail.com>
2020-08-09 15:13:46 +0900

    Added Network Interface

--- src/dev/durok/main/Server_Prototype.java
+++ src/dev/durok/main/Server_Prototype.java
@@ -1,7 +1,11 @@
 package dev.durok.main;
 
+import dev.durok.network.Network;
+
 public class Server_Prototype {
     public static void main ( String[] argv ) {
+        Network network = new Network("Server Prototype");
 
+        System.out.println(network.GetApplicationSummary());
     }
 }
--- src/dev/durok/network/Network.java
+++ src/dev/durok/network/Network.java
@@ -1,4 +1,32 @@
 package dev.durok.network;
 
-public class Network {
+
+import dev.durok.network.Interface.INetwork;
+
+public class Network implements INetwork {
+    final private String mApplicationSummary;
+
+    public Network ( String applicationSummary ) {
+        mApplicationSummary = applicationSummary;
+    }
+
+    public String GetApplicationSummary( ) {
+        return mApplicationSummary;
+    }
+
+    @Override
+    public boolean startService() {
+
+        return true;
+    }
+
+    @Override
+    public boolean stopService() {
+        return true;
+    }
+
+    @Override
+    public String getStatus() {
+        return null;
+    }
 }
