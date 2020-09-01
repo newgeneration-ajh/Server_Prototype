5c81ed8e3f94a51797e6dae3471f2bc89f2bab95
jihunahn <angh5080@gmail.com>
2020-09-02 00:31:05 +0900

    Server 객체 테스트를 위하여, Main에 추가함

--- src/dev/durok/server_prototype/main/Server_Prototype.java
+++ src/dev/durok/server_prototype/main/Server_Prototype.java
@@ -1,11 +1,12 @@
 package dev.durok.server_prototype.main;
 
 import dev.durok.server_prototype.network.Network;
+import dev.durok.server_prototype.network.Server;
 
 public class Server_Prototype {
     public static void main ( String[] argv ) {
         Network network = new Network("Server Prototype");
-
+        Server server = new Server ( 12030 );
         System.out.println(network.GetApplicationSummary());
     }
 }
